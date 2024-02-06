7e966db7e6a1475cc05a29290dc9d5a0b0b3889b
daw1 - Carballo Puebla <A23OscarCP@sanclemente.local>
2024-02-06 10:11:19 +0100

    sync class

---
+++ UD4/a.py
@@ -0,0 +1,9 @@
+#Escribe aquí el código de tu solución
+a = input("Dime una frase: ")
+c = input("Dime un caracter: ")
+a=a.replace("a",c)
+a=a.replace("e",c)
+a=a.replace("i",c)
+a=a.replace("o",c)
+a=a.replace("u",c)
+print(a)
